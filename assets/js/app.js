45d51e5744442c8d3ab1ae24c282245414070838
Bastien CLEMENT <bclement@assurone-group.com>
2020-08-01 19:12:52 +0200

    Refactoring Template Twig (home)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,2 @@
-const $ = require('jquery');
-// this "modifies" the jquery module: adding behavior to it
-// the bootstrap module doesn't export/return anything
-import '../css/global.scss';
-
-// or you can include specific pieces
-// require('bootstrap/js/dist/tooltip');
-// require('bootstrap/js/dist/popover');
-
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
+require('../css/global.scss');
+require('bootstrap');
